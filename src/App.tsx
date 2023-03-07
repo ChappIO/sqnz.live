40e33bc65fbceb4f7a17782b69025e1de99b9b24
Thomas Biesaart <thomas.biesaart@protonmail.com>
2023-03-08 00:41:36 +0100

    Fix another issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import {AudioContextProvider} from "./hooks/useAudioContext";
 import {Expresso} from "./instruments/Expresso";
 
-export const App = ({}) => {
+export const App = () => {
     return (
         <AudioContextProvider>
             <PlayButton>
