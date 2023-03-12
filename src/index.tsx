69911ededc3483ee6ff704280cf51ab655054dd9
Thomas Biesaart <thomas.biesaart@protonmail.com>
2023-03-12 12:43:33 +0100

    Enable PWA

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import {App} from "./App";
 import "./reset.scss";
 import "papercss";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(<App/>);
+
+serviceWorkerRegistration.register();
